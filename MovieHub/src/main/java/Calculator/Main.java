243bed4ad7054df2db4bb34cf417e1c36fabe2bf
nasif-zaman38 <nasif.uzzaman.38@gmail.com>
2026-09-22 22:24:29 +0600

    Fix review suggestions

--- MovieHub/src/main/java/Calculator/Main.java
+++ MovieHub/src/main/java/Calculator/Main.java
@@ -2,10 +2,10 @@ package Calculator;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator c=new Calculator();
-        System.out.println("Sum of two integers: "+c.add(20,10));
-        System.out.println("Sum of three integers: "+c.add(20, 10, 5));
-        System.out.println("Sum of two decimal numbers: "+c.add(10.5, 7.9));
-        System.out.println("Sum of integer and decimal number: "+c.add(10,5.5));
+        Calculator c = new Calculator();
+        System.out.println("Sum of two integers: " + c.add(20,10));
+        System.out.println("Sum of three integers: " + c.add(20, 10, 5));
+        System.out.println("Sum of two decimal numbers: " + c.add(10.5, 7.9));
+        System.out.println("Sum of integer and decimal number: " + c.add(10,5.5));
     }
 }
